da779309f9267be24d787e72cd5888f809a6283f
DeividMesquita <deividsond08@gmail.com>
2025-04-10 09:29:13 -0300

    Update: melhoria na dica visual de sombra

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -3,78 +3,28 @@ const data = [
         status: "To Do",
         color: "#fff",
         items: [
-            {
-                id: 1,
-                title: "Obtaining the girdle of Hippolyta, queen of the Amazons",
-            },
-            {
-                id: 2,
-                title: "Obtaining the cattle of the three-bodied giant Geryon",
 
-            },
-            {
-                id: 3,
-                title: "Stealing three of the golden apples of the Hesperides",
-
-            },
-            {
-                id: 4,
-                title: "Capturing and bringing back Cerberus",
-
-            }
         ]
     },
     {
         status: "Doing",
         color: "#2D8EFF",
         items: [
-            {
-                id: 1,
-                title: "Task 1",
 
-            },
-            {
-                id: 2,
-                title: "Task 2",
-
-            }
         ]
     },
     {
         status: "Testing",
         color: "#FFCC3D",
         items: [
-            {
-                id: 1,
-                title: "Task 1",
 
-            },
-            {
-                id: 2,
-                title: "Task 2",
-
-            },
-            {
-                id: 3,
-                title: "Task 3",
-
-            }
         ]
     },
     {
         status: "Done",
         color: "#13CE66",
         items: [
-            {
-                id: 1,
-                title: "Task 1",
 
-            },
-            {
-                id: 2,
-                title: "Task 2",
-
-            }
         ]
     }
 ]
